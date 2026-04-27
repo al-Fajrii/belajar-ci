3c47aefbc1e9d59eec528e95777462cfa6d3dc3c
Muhammaf Syafiq Al Fajri <muhammadsyafiq0625@gmail.com>
2026-04-27 15:56:48 +0700

    UTS_PWL

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -18,7 +18,7 @@ public function login()
         $password = $this->request->getVar('password');
 
         $dataUser = [
-            'username' => 'april',
+            'username' => 'danny',
             'password' => '202cb962ac59075b964b07152d234b70',
             'role' => 'admin',
             'email' => 'april@gmail.com'
